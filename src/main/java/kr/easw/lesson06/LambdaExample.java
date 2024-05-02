3cb1b2fa0ffbde0997460d86d061b2c66c23780e
suchwoon <115792419+suchwoon@users.noreply.github.com>
2024-05-02 20:56:58 +0900

    Update LambdaExample.java

--- src/main/java/kr/easw/lesson06/LambdaExample.java
+++ src/main/java/kr/easw/lesson06/LambdaExample.java
@@ -21,7 +21,7 @@ public class LambdaExample {
     // Supplier은 제너릭스에 포함된 타입을 반환하는 함수형 인터페이스입니다.
     // 이는 다음과 같은 코드를 축약한 형태입니다 :
     // public int supplyNumber() {
-    //    return numbers[RANDOM.nextInt(numbers.length)];
+    // return numbers[RANDOM.nextInt(numbers.length)];
     // }
     private static final Supplier<Integer> numberSupplier = () -> numbers[RANDOM.nextInt(numbers.length)];
 
@@ -29,7 +29,7 @@ public class LambdaExample {
     // 이 람다 제너릭스는 <Integer, Integer>이기에 Integer를 받아 Integer를 반환합니다.
     // 해당 람다 표현식은 다음과 같은 코드를 축약한 형태입니다 :
     // public int convertToNegative(int number) {
-    //    return -number;
+    // return -number;
     // }
     private static final Function<Integer, Integer> negativeConverter = (number) -> -number;
 
@@ -37,7 +37,7 @@ public class LambdaExample {
     // 이 람다 제너릭스는 <Integer>이기에 Integer를 받습니다.
     // 해당 람다 표현식은 다음과 같은 코드를 축약한 형태입니다 :
     // public void printNumber(int number) {
-    //    System.out.println(number);
+    // System.out.println(number);
     // }
     @SuppressWarnings({"Convert2MethodRef", "RedundantSuppression"})
     private static final Consumer<Integer> printer = (number) -> {
@@ -48,17 +48,18 @@ public class LambdaExample {
     // 이 람다 제너릭스는 <Integer, Integer, Integer>이기에 Integer를 두 개 받아 Integer를 반환합니다.
     // 해당 람다 표현식은 다음과 같은 코드를 축약한 형태입니다 :
     // public int adjustNumber(int number, int adjustment) {
-    //    return number + adjustment;
+    // return number + adjustment;
     // }
     @SuppressWarnings({"Convert2MethodRef", "RedundantSuppression"})
     private static final BiFunction<Integer, Integer, Integer> adjuster = (number, adjustment) -> number + adjustment;
 
-
     public static void main(String[] args) {
         int first = numberSupplier.get();
         int second = numberSupplier.get();
+
         int example = exampleResult(first, second);
         int lambda = lambdaResult(first, second);
+
         if (example == lambda) {
             System.out.println("정답입니다!");
         } else {
@@ -74,12 +75,9 @@ public class LambdaExample {
         return adjusted;
     }
 
-    // 이 메서드는 exampleResult와 동일한 동작을 위에 선언된 람다 표현식을 사용하여 수행해야 합니다.
-    // 각 람다는 서로 다른 이름으로 값을 수용한다는것을 잊지 마세요.
-    // Function / BiFunction은 apply를, Consumer는 accept를 사용합니다.
     private static int lambdaResult(int base, int delta) {
-
-        throw new UnsupportedOperationException("이 코드를 지우고, 정답을 작성하세요.");
+        int negative = negativeConverter.apply(delta);
+        printer.accept(negative);
+        return adjuster.apply(base, delta);
     }
-
 }
